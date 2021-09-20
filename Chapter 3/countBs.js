88e424137e28b6f6975c340701c41b402a21172c
aigurov <gurov@byndyusoft.com>
2021-09-20 21:23:50 +0500

    count upper chars

--- Chapter 3/countBs.js
+++ Chapter 3/countBs.js
@@ -1,5 +1,12 @@
-const countBs = (a) => {
+const countBs = (string) => {
+    let count = 0;
 
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i].toUpperCase()){
+            count++;
+        }
 
+    }
+    return count;
 };
-console.log(countBs());
+console.log(countBs(''));
